d5c9a6f2c5e57e3e5e4590ab642642249ca9dc6a
globekeeper <valerian@globekeeper.com>
2021-07-08 16:43:18 +0300

    Add Json Tag to properties

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/media/GKLocation.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/media/GKLocation.kt
@@ -1,12 +1,15 @@
 package org.matrix.android.sdk.internal.session.media
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @JsonClass(generateAdapter = true)
-data class GKLocation(val type: String, val geometry: Geometry, val properties: Properties) : Parcelable
+data class GKLocation(val type: String,
+                      @Json(name = "geometry") val geometry: Geometry,
+                      @Json(name = "properties") val properties: Properties) : Parcelable
 
 @Parcelize
 @JsonClass(generateAdapter = true)
