4331c0bc6cc0f3f6c4f6ccf24ab3949d31e6419b
Benoit Marty <benoit@matrix.org>
2021-08-30 14:31:28 +0200

    Import 1.2.0 from Element

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/model/message/AudioWaveformInfo.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/model/message/AudioWaveformInfo.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,20 +19,18 @@ package org.matrix.android.sdk.api.session.room.model.message
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+/**
+ * See https://github.com/matrix-org/matrix-doc/blob/travis/msc/audio-waveform/proposals/3246-audio-waveform.md
+ */
 @JsonClass(generateAdapter = true)
-data class AudioInfo(
-        /**
-         * The mimetype of the audio e.g. "audio/aac".
-         */
-        @Json(name = "mimetype") val mimeType: String? = null,
+data class AudioWaveformInfo(
+        @Json(name = "duration")
+        val duration: Int? = null,
 
         /**
-         * The size of the audio clip in bytes.
+         * The array should have no less than 30 elements and no more than 120.
+         * List of integers between zero and 1024, inclusive.
          */
-        @Json(name = "size") val size: Long? = null,
-
-        /**
-         * The duration of the audio in milliseconds.
-         */
-        @Json(name = "duration") val duration: Int? = null
+        @Json(name = "waveform")
+        val waveform: List<Int>? = null
 )
